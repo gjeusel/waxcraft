fd95b6d401f0dd6bf35a52db2f9021d217bfdbd0
Guillaume Jeusel <jeusel.guillaume@gmail.com>
2016-02-08 11:24:02 +0100

    Add of vim-conf bash-conf nixpkgs & config
    Signed-off-by: Guillaume Jeusel <jeusel.guillaume@gmail.com>

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ Myconfig
 This repository contain my config files for :
  - vim
  - bash
+ - nixpkgs
+ - kde
 
+vim-conf/* ---> vim-conf/.*
+bash-conf/*  ---> bash-conf/.*
+nixpkgs/  ---> .nixpkgs/
---
+++ nixpkgs/config.nix
@@ -0,0 +1,52 @@
+{
+
+# Regarding configs
+firefox = {
+  enableAdobeFlash = true;
+  enableGoogleTalkPlugin = true;
+};
+
+chromium = {
+  enableGoogleTalkPlugin = true;
+  enablePepperFlash = true; # Chromium's non-NSAPI alternative to Adobe Flash
+  enablePepperPDF = true;
+};
+
+packageOverrides = super: let self = super.pkgs; in with self; rec {
+
+  pathToMyPackages = "/root/projects/pumped-nix/" ;
+  vim74-spf13 = self.callPackage "${pathToMyPackages}vim-7.4-spf13/default.nix" {  };
+  blank_env   = self.callPackage "${pathToMyPackages}blank_env/default.nix" {  };
+
+  /*lmod        = self.callPackage "${pathToMyPackages}lmod/default.nix" {*/
+  /*    inherit (self.luaPackages) luafilesystem;*/
+  /*    inherit luaposix;*/
+  /*};*/
+
+};
+
+/*packageOverrides = super: rec {*/
+
+/*# super = paquet des dépot de départ*/
+/*# on peut renommer super en nixpkgs*/
+
+/*# Pour faire une référence au package vim dans les paquets officiels de nix :*/
+/*  vim = super*/
+
+/*  pathToMyPackages = "/root/projects/pumped-nix/" ;*/
+/*  vim74-spf13 = super.pkgs.callPackage "${pathToMyPackages}vim-7.4-spf13/default.nix" {  };*/
+/*  blank_env   = super.pkgs.callPackage "${pathToMyPackages}blank_env/default.nix" {  };*/
+
+/*  lmod        = self.callPackage "${pathToMyPackages}lmod/default.nix" {*/
+/*      inherit (self.luaPackages) luafilesystem;*/
+/*      inherit luaposix;*/
+/*  };*/
+
+/*};*/
+
+# Allow broken content (when using nixpkgs as git)
+allowBroken = true;
+# Allow unfree license
+allowUnfree = true;
+
+}
