6977c6f1ef2e6e40ad8fed837d74b42ac7980ba2
Guillaume Jeusel <guillaume.jeusel@gmail.com>
2017-10-18 23:26:35 +0200

    setup.py reworked
    Signed-off-by: Guillaume Jeusel <guillaume.jeusel@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys, os, re
 reload(sys)
 sys.setdefaultencoding('utf8') # problem with encoding
 import subprocess
-import datetime
+from datetime import datetime
 
 import shutil as sh
 
@@ -21,188 +21,203 @@ home          = os.environ['HOME'] + '/'
 config_dir    = home + '.config/'
 # config_dir    = os.environ['XDG_HOME_DIR'] + '/'
 
-old_conf_dir = waxCraft_path + '/.old-conf/'
 
-bash_cfg_dir   = waxCraft_path + '/bash-conf/'
-vim_cfg_dir    = waxCraft_path + '/vim-conf/'
-plasma_cfg_dir = waxCraft_path + '/kde-plasma-conf/'
-nixpkgs_cfg_dir= waxCraft_path + '/nix/nixpkgs/'
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
 
 
-class bash_cfg_class:
-#{{{
-    def __init__(self):
-        print '--- Installing Bash conf ---'
-        self.nml = ['.bash_aliases', '.inputrc']
+class waxCraft:
+    old_conf_dir   = waxCraft_path + '/.old-conf/'
 
-        now = datetime.datetime.now()
-        old_conf_bash_dir = old_conf_dir + 'bash-conf-'
-        old_conf_bash_dir+= str(now.day)+'.'+str(now.month)+'.'+str(now.year)+'.'
-        old_conf_bash_dir+= str(now.hour)+'.'+str(now.minute)
-        old_conf_bash_dir+= '/'
+    bash_cfg_dir   = waxCraft_path + '/bash-conf/'
+    vim_cfg_dir    = waxCraft_path + '/vim-conf/'
+    plasma_cfg_dir = waxCraft_path + '/kde-plasma-conf/'
+    nixpkgs_cfg_dir= waxCraft_path + '/nix/nixpkgs/'
 
-        print 'Making a backup of your bash config files in ',\
-              old_conf_bash_dir, ' ...'
-        self.save_old_cfg(old_conf_bash_dir)
+    def __init__(self, cfg_list):
+        self.cfg_list = cfg_list
 
-        print 'Creating symlinks ...'
-        for e in self.nml:
-            if os.path.exists(home + e):
-                os.remove(home + e)
-            os.symlink(bash_cfg_dir + e, home + e)
+        now = datetime.now()
+        now_str = now.strftime('%Y-%m-%d-%H-%M')
 
-        print 'Adding source bashrc_common.sh to ~/.bashrc file ...'
-        self.add_source_to_bashrc()
+        dict_cfg = {
+            'vim': dict(config_dir = self.vim_cfg_dir,
+                        old_conf_dir = self.old_conf_dir+'vim-'+now_str,
+                        nml = ['.vimrc.local',
+                               '.vimrc.before.local',
+                               '.vimrc.bundles.local'],
+                       ),
 
-#{{{
-    def save_old_cfg(self, old_conf_bash_dir):
+            'bash': dict(config_dir = self.bash_cfg_dir,
+                         old_conf_dir=self.old_conf_dir+'bash-'+now_str,
+                         nml = ['.bash_aliases', '.inputrc'],
+                        ),
+
+            'plasma': dict(config_dir = self.plasma_cfg_dir,
+                           old_conf_dir = self.old_conf_dir+'plasma-'+now_str,
+                           nml = ['kglobalshortcutsrc',
+                                  'ksmserverrc',
+                                  'kwinrc',
+                                  'khotkeysrc',
+                                  'kwalletrc',
+                                  'plasma-org.kde.plasma.desktop-appletsrc',
+                                  'xfce4/terminal/terminalrc'
+                                 ]
+                           ),
+        }
+
+        for k in dict_cfg.keys():
+            dict_cfg[k]['nml_file_to_backup'] = dict_cfg[k]['nml']
+
+        dict_cfg['vim']['nml_file_to_backup'] += ['.vimrc', '.vimrc.before',
+                                                  '.vimrc.bundles']
+
+        self.dict_cfg = dict_cfg
+
+    def save_old_cfg(self, cfg):
+        print('Backup in ' + self.dict_cfg[cfg]['old_conf_dir'] + ' ...')
         try:
-            os.makedirs(old_conf_bash_dir)
+            os.makedirs(self.dict_cfg[cfg]['old_conf_dir'])
         except OSError as exc: # Python >2.5
             if exc.errno != errno.EEXIST:
                 raise
 
-        for e in self.nml :
+        for e in self.dict_cfg[cfg]['nml_file_to_backup']:
             if os.path.exists(home + e):
-                sh.copyfile(home + e, old_conf_bash_dir + e)
+                sh.copyfile(home + e, self.dict_cfg[cfg]['old_conf_dir'] + e)
 
-    def add_source_to_bashrc(self):
+    def create_symlinks(self, cfg):
+        print('Creating symlinks ...')
+        for e in self.dict_cfg[cfg]['nml']:
+            try:
+                os.remove(home + e)
+            except OSError as exc:
+                if exc.errno != 2: # means that no such file or directory
+                    raise
+            os.symlink(self.dict_cfg[cfg]['config_dir'] + e, home + e)
+
+    def copy_files(self, cfg):
+        print('Copying files ...')
+        for e in self.dict_cfg[cfg]['nml']:
+            src = self.dict_cfg[cfg]['config_dir'] + e
+            dest = home + '.config/' + e
+
+            # Remove existing file
+            try:
+                os.remove(dest)
+            except OSError as exc:
+                if exc.errno != 2: # means that no such file or directory
+                    raise
+
+            # Create directories if not already existing
+            alr_exist = bool(os.path.exists(os.path.dirname(dest)))
+            if (not alr_exist):
+                try:
+                    os.makedirs(os.path.dirname(dest))
+                except OSError as exc: # Guard against race condition
+                    if exc.errno != errno.EEXIST:
+                        raise
+
+            # copy file :
+            sh.copy(src, dest)
+
+
+    def install_vim(self):
+        print('--- Installing Vim conf ---')
+        self.save_old_cfg('vim')
+
+        def install_vundle():
+            try:
+                retcode = subprocess.call("git --version &> /dev/null",
+                                        shell=True)
+            except OSError as exc:
+                if exc.errno != 0 : # means that git isn't accessible
+                    raise
+
+            recode = subprocess.call("git clone " +
+                "https://github.com/VundleVim/Vundle.vim.git " +
+                ".vim/bundle/Vundle.vim", shell=True)
+
+
+        def install_spf13():
+            # retcode = subprocess.call("curl http://j.mp/spf13-vim3 -L -o - | sh",
+            #                         shell=True)
+            pass
+
+        if not os.path.exists(home+'.spf13-vim-3/'):
+            install_spf13()
+
+        self.create_symlinks('vim')
+
+    def install_bash(self):
+        print('--- Installing Bash conf ---')
+        self.save_old_cfg('bash')
+        self.create_symlinks('bash')
+
+        # Adding line in ~/.bashrc to source baschrc_common.sh
         str_source = '# waxCraft bashrc_common.sh file sourcing :\n' +\
-                     'source ' + bash_cfg_dir + 'bashrc_common.sh'
+                     'source ' + self.dict_cfg['bash']['config_dir'] + 'bashrc_common.sh'
 
         if os.path.exists(home+'.bashrc'):
             if str_source not in open(home+'.bashrc').read():
                 open(home+'.bashrc', 'a').write('\n'+str_source)
         else :
             open(home+'.bashrc', 'w').write(str_source)
-#}}}
 
-#}}}
+    def install_plasma(self):
+        print '--- Installing Plasma conf ---'
+        self.save_old_cfg('plasma')
+        self.copy_files('plasma')
 
-class vim_cfg_class:
-#{{{
-    def __init__(self):
-        print '--- Installing Vim conf ---'
-        self.nml = ['.vimrc.local', '.vimrc.before.local',
-                    '.vimrc.bundles.local']
-
-        now = datetime.datetime.now()
-        old_conf_vim_dir = old_conf_dir + 'vim-conf-'
-        old_conf_vim_dir+= str(now.day)+'.'+str(now.month)+'.'+str(now.year)+'.'
-        old_conf_vim_dir+= str(now.hour)+'.'+str(now.minute)
-        old_conf_vim_dir+= '/'
-        print 'Making a backup of your vim config files in ',\
-              old_conf_vim_dir, ' ...'
-        self.save_old_cfg(old_conf_vim_dir)
-
-        spf13_path = home + '.spf13-vim-3/'
-        if not os.path.exists(spf13_path):
-            print 'Installing spf13 ...'
-            self.install_spf13()
-
-        print 'Creating symlinks ...'
-        for e in self.nml:
-            if os.path.exists(home + e):
-                os.remove(home + e)
-            os.symlink(vim_cfg_dir + e, home + e)
-        for e in ['.vimrc', '.vimrc.before', '.vimrc.bundles']:
-            if os.path.exists(home + e):
-                os.remove(home + e)
-            os.symlink(spf13_path + e, home + e)
-
-#{{{
-    def save_old_cfg(self, old_conf_vim_dir):
-        nml_file_to_backup = self.nml + ['.vimrc', '.vimrc.before',
-                                         '.vimrc.bundles']
-        try:
-            os.makedirs(old_conf_vim_dir)
-        except OSError as exc: # Python >2.5
-            if exc.errno != errno.EEXIST:
-                raise
-
-        for e in nml_file_to_backup :
-            if os.path.exists(home + e):
-                sh.copyfile(home + e, old_conf_vim_dir + e)
-
-    def install_spf13(self):
-        try:
-            retcode = subprocess.call('curl --version &> /dev/null',
-                                      shell=True)
-        except OSError as exc:
-            if exc.errno != 0 : # means that git isn't accessible
-                print 'ERROR : curl bash cmd is required for spf13 install.'
-                raise
-
-        retcode = subprocess.call("curl http://j.mp/spf13-vim3 -L -o - | sh",
-                                  shell=True)
-
-    def download_vundle(self):
-        try:
-            retcode = subprocess.call("git --version &> /dev/null",
+        quest = 'Session need to restart, are your sure you want to quite'
+        choice = query_yes_no(quest)
+        if choice == 'yes':
+            retcode = subprocess.call("loginctl terminate-user " +
+                                    str(os.environ['USER']),
                                     shell=True)
-        except OSError as exc:
-            if exc.errno != 0 : # means that git isn't accessible
-                raise
 
-        recode = subprocess.call("git clone " +
-            "https://github.com/VundleVim/Vundle.vim.git " +
-            ".vim/bundle/Vundle.vim", shell=True)
-#}}}
 
-#}}}
+    def install(self):
+        if 'vim' in self.cfg_list:
+            self.install_vim()
 
-class kde_plasma_class:
-#{{{
-    def __init__(self):
-        print "--- Installing KDE's Plasma conf ---"
-        self.nml = [
-            'kglobalshortcutsrc',
-            'ksmserverrc',
-            'kwinrc',
-            'khotkeysrc',
-            'kwalletrc',
-            'plasma-org.kde.plasma.desktop-appletsrc'
-        ]
+        if 'bash' in self.cfg_list:
+            self.install_bash()
 
-        now = datetime.datetime.now()
-        old_conf_kde_plasma_dir = old_conf_dir + 'kde-plasma-conf-'
-        old_conf_kde_plasma_dir+= str(now.day)+'.'+str(now.month)+'.'+str(now.year)+'.'
-        old_conf_kde_plasma_dir+= str(now.hour)+'.'+str(now.minute)
-        old_conf_kde_plasma_dir+= '/'
-        print 'Making a backup of your kde plasma config files in ',\
-              old_conf_kde_plasma_dir, ' ...'
-        self.save_old_cfg(old_conf_kde_plasma_dir)
-
-        print 'Copying new config files ...'
-        for e in self.nml:
-            if os.path.exists(config_dir + e):
-                os.remove(config_dir + e)
-            sh.copyfile(plasma_cfg_dir + e, config_dir + e)
-
-        print 'Restarting Xorg and Plasma by disconnecting ...'
-        retcode = subprocess.call("loginctl terminate-user " +
-                                  str(os.environ['USER']),
-                                  shell=True)
-
-#{{{
-    def save_old_cfg(self, old_conf_kde_plasma_dir):
-        try:
-            os.makedirs(old_conf_kde_plasma_dir)
-        except OSError as exc: # Python >2.5
-            if exc.errno != errno.EEXIST:
-                raise
-
-        for e in self.nml :
-            if os.path.exists(config_dir + e):
-                sh.copyfile(config_dir + e, old_conf_kde_plasma_dir + e)
-
-#}}}
-
-#}}}
+        if 'plasma' in self.cfg_list:
+            self.install_plasma()
 
 
 class nixpkgs_class:
-#{{{
     def __init__(self):
         print "--- Installing nixpkgs conf ---"
         self.nml = ['.nixpkgs/config.nix']
@@ -211,15 +226,14 @@ class nixpkgs_class:
             os.makedirs(home+'.nixpkgs')
         for e in self.nml:
             os.symlink(nixpkgs_cfg_dir + 'custom-packages.nix' , home + e)
-#}}}
+
 
 def setup_argparser():
-#{{{
     """ Define and return the command argument parser. """
     parser = argparse.ArgumentParser(description='''waxCraft config setup.''')
 
-    parser.add_argument('cfg_list', nargs='+',
-                        choices=['bash', 'vim', 'plasma', 'nixpkgs'],
+    parser.add_argument('cfg_list', nargs='+', choices=['bash', 'vim',
+                                                        'plasma', 'nixpkgs'],
                         help='''cfg to install''')
 
     parser.add_argument('-v', '--verbose', dest='verbose', required=False,
@@ -227,7 +241,6 @@ def setup_argparser():
                         help='Verbose level: 0 for errors, 1 for info or 2 for debug.')
 
     return parser
-#}}}
 
 
 def main(argv=None):
@@ -240,19 +253,11 @@ def main(argv=None):
     except argparse.ArgumentError as exc:
         raise
 
-    # verbose  = args.verbose
+    wax = waxCraft(args.cfg_list)
+    wax.install()
 
-    if 'bash' in args.cfg_list :
-        bash_cfg = bash_cfg_class()
-
-    if 'vim' in args.cfg_list :
-        vim_cfg = vim_cfg_class()
-
-    if 'plasma' in args.cfg_list :
-        plasma_cfg = kde_plasma_class()
-
-    if 'nixpkgs' in args.cfg_list :
-        nixpkgs_cfg = nixpkgs_class()
+    # if 'nixpkgs' in args.cfg_list :
+    #     nixpkgs_cfg = nixpkgs_class()
 
 
 if __name__ == "__main__":
