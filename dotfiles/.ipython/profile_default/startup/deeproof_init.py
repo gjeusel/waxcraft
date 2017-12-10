7ec575d39e9ec511504c64aa44d4a83768532b57
Guillaume <jeusel.guillaume@gmail.com>
2017-12-10 16:43:08 +0100

    ipython config files
    Signed-off-by: Guillaume <jeusel.guillaume@gmail.com>

---
+++ dotfiles/.ipython/profile_default/startup/deeproof_init.py
@@ -0,0 +1,19 @@
+try:
+    import numpy as np
+except ImportError:
+    pass
+
+from os.path import expanduser
+HOME = expanduser("~")
+
+try:
+    from handle_data import *
+    images = load_raw_images(n_limit=200)
+except ImportError:
+    pass
+
+try:
+    from model import *
+    # m = wrapper_model(n_limit=1000)
+except ImportError:
+    pass
