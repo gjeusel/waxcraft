828b5add1555d47f20f8fad8aeb813f4e96bf28c
Guillaume Jeusel <guillaume.jeusel@engie.com>
2019-03-30 12:29:49 +0100

    Remove nix as security alerts

--- os/nix/nix-shell/setup_campeas_packages.sh
+++
@@ -1,3 +0,0 @@
-python inireader/setup.py install --user
-python pymercure/setup.py develop --user
-python tshistory/setup.py develop --user
--- os/nix/nixpkgs/blank_env/default.nix
+++
@@ -1,7 +0,0 @@
-{ stdenv, python, cmake }:
-
-stdenv.mkDerivation rec {
-  name = "blank_env";
-
-  buildInputs = [ python cmake];
-}
