e1be6bbc10dffc6ad09f175c8b5c416641eae9a4
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2020-06-18 11:58:55 +0200

    [PTPYTHON] cleanup startup

--- dotfiles/.python_startup.py
+++ dotfiles/.python_startup.py
@@ -3,6 +3,7 @@ import importlib
 import json
 import logging
 import os
+import os.path as osp
 import sys
 from collections import defaultdict
 from datetime import datetime, timedelta
@@ -16,34 +17,35 @@ import __main__
 
 TZ = pytz.timezone("Europe/Brussels")
 
-log = logging.basicConfig(
-    format="%(asctime)s %(levelname)s:%(message)s",
-    level=logging.INFO,
-    datefmt="%I:%M:%S",
-)
 
-logroot = logging.getLogger("")
-logroot.setLevel(logging.INFO)
+def setup_repl_logs():
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s:%(message)s",
+        level=logging.INFO,
+        datefmt="%I:%M:%S",
+    )
 
-logging.getLogger("restless-client").setLevel(logging.DEBUG)
+    logroot = logging.getLogger("")
+    logroot.setLevel(logging.INFO)
 
-logging.getLogger("parso.python.diff").disabled = True
+    for lib in ("parso.python.diff", "parso.cache", "parso.cache.pickle"):
+        logging.getLogger(lib).disabled = True
 
-# Avoid logging repport when auto-complete with tab
-# cd https://stackoverflow.com/questions/49442523/prevent-showing-debugging-log-info-inside-ipython-shell
-logging.getLogger("parso.cache").disabled = True
-logging.getLogger("parso.cache.pickle").disabled = True
+    warning_libs = ("requests", "urllib3", "parso", "diff", "pickle", "cache")
+    for lib in warning_libs:
+        logging.getLogger(lib).setLevel(logging.WARNING)
 
-for lib in [
-        "requests", "urllib3", "mercure", "parso", "diff", "pickle", "cache"
-]:
-    logging.getLogger(lib).setLevel(logging.WARNING)
+    urllib3.disable_warnings()
 
-urllib3.disable_warnings()
+
+setup_repl_logs()
+
 
 try:
     import pandas as pd
-
+except ImportError:
+    pass
+else:
     # Recent Dates aware:
     now = pd.Timestamp.now(tz="CET").floor("s")
     end = now + pd.Timedelta(hours=4)
@@ -87,9 +89,8 @@ try:
         idx = pd.date_range(start=start, freq=freq, periods=size)
         return pd.Series([1.0] * len(idx), index=idx)
 
-except ImportError:
-    pass
 
+# Compile and Exec local config
 local_startup_file = Path.home() / ".python_startup_local.py"
 if local_startup_file.exists():
     filepath = local_startup_file.as_posix()
@@ -97,48 +98,64 @@ if local_startup_file.exists():
         compiled = compile(f.read(), filepath, "exec")
     exec(compiled, __main__.__dict__, __main__.__dict__)
 
-imports = (
-    "ticts",
-    "requests",
-    "flask",
-    ("pdops", "arkolor.pdops"),
-    ("sa", "sqlalchemy"),
-    "sqlalchemy.orm",  # else sa.orm not available
-    ("m", "zefire.models"),
-)
-for imp in imports:
-    alias = None
-    if isinstance(imp, (tuple, list)):
-        alias, imp = imp
-    alias = alias or imp
+
+def import_in_ctx(pkg, alias=None):
+    if isinstance(pkg, (tuple, list)):
+        alias, pkg = pkg
+    alias = alias or pkg
     try:
-        __main__.__dict__[alias] = importlib.import_module(imp)
+        __main__.__dict__[alias] = importlib.import_module(pkg)
     except ImportError:
         pass
 
+
+imports = (
+    "requests",
+    "flask",
+    # ("pdops", "arkolor.pdops"),
+    # ("sa", "sqlalchemy"),
+    # "sqlalchemy.orm",  # else sa.orm not available
+    # ("m", "zefire.models"),
+)
+for pkg in imports:
+    if isinstance(pkg, (tuple, list)):
+        import_in_ctx(*pkg)
+    else:
+        import_in_ctx(pkg)
+
+
+_ptpython_conf_dir = Path.home() / ".ptpython"
+
+
+def _deduce_history_file():
+    cmd = __main__.__file__
+
+    #  If entered by flask shell:
+    if cmd.endswith("/bin/flask"):
+        return _ptpython_conf_dir / "flask_ctx_history"
+    else:
+        return _ptpython_conf_dir / "history"
+
+
+def _ptpython_configure(repl):
+    from ptpython.repl import run_config
+
+    path = _ptpython_conf_dir / "config.py"
+    if path.exists():
+        run_config(repl, path.as_posix())
+
+
 try:
-    from ptpython.repl import embed, run_config
+    from ptpython.repl import embed
 except ImportError:
     pass
 else:
-    localctx = __main__.__dict__
-    _should_enter = all([
-        "get_ipython" not in localctx,  # if not run from ipython
-    ])
-    if _should_enter:
-        confdir = Path.home() / ".ptpython"
-
-        # Apply config file
-        def configure(repl):
-            path = confdir / "config.py"
-            if path.exists():
-                run_config(repl, path.as_posix())
-
-        history_path = confdir / "history"
+    # if not run from ipython
+    if "get_ipython" not in __main__.__dict__:
         new = embed(
-            history_filename=history_path,
-            configure=configure,
-            locals=localctx,
+            history_filename=_deduce_history_file().as_posix(),
+            configure=_ptpython_configure,
+            locals=__main__.__dict__,
             globals=globals(),
             title="Python REPL (ptpython)",
         )
