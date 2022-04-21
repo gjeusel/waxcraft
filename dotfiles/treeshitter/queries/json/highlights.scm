a1b45140ef40de6d04115999a7c58b8014623423
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2022-04-21 14:00:56 +0200

    [NVIM] treeshitter don't conceal " for json

---
+++ dotfiles/treeshitter/queries/json/highlights.scm
@@ -0,0 +1,17 @@
+(true) @boolean
+(false) @boolean
+(null) @constant.builtin
+(number) @number
+(pair key: (string) @label)
+(pair value: (string) @string)
+(array (string) @string)
+(string_content (escape_sequence) @string.escape)
+(ERROR) @error
+["," ":"] @punctuation.delimiter
+"[" @punctuation.bracket
+"]" @punctuation.bracket
+"{" @punctuation.bracket
+"}" @punctuation.bracket
+
+;; Don't conceal "  !!!
+;; (("\"" @conceal) (#set! conceal ""))
