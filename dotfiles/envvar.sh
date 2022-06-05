71379f98e1d6f72caaacf524e1a75979b39e53a1
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2022-06-05 08:01:06 +0200

    [SH] remove shitty nvm and use Volta

--- dotfiles/envvar.sh
+++ dotfiles/envvar.sh
@@ -33,13 +33,3 @@ export VIM_MODE_TRACK_KEYMAP=no  # else it makes the terminal bug
 
 # Enhancd
 export ENHANCD_DISABLE_HYPHEN=1  # disable enhancd for "cd -"
-
-# Lazy NVM (NodeJS)
-export NVM_COMPLETION=false
-if [ $TERM_PROGRAM = vscode ]; then
-  export NVM_LAZY_LOAD=false
-else
-  # too slow, maybe depending on the directory ?
-  # export NVM_LAZY_LOAD_EXTRA_COMMANDS=("neovim", "vim", "nvim")
-  export NVM_LAZY_LOAD=true
-fi
