83ec13855dc9fda597f5691d456a74ba9373b092
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2022-10-26 11:19:24 +0200

    imp(lsp) sumneko

--- dotfiles/.config/nvim/after/ftplugin/python.lua
+++ dotfiles/.config/nvim/after/ftplugin/python.lua
@@ -2,6 +2,8 @@ vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 vim.opt_local.softtabstop = 4
 
+vim.o.autoindent = true -- use previous line indent
+
 vim.g.python_indent = {
   open_paren = "&sw",
   nested_paren = "&sw",
--- dotfiles/.config/nvim/lua/wax/lsp/sumneko_lua-ls.lua
+++ dotfiles/.config/nvim/lua/wax/lsp/sumneko_lua-ls.lua
@@ -2,18 +2,43 @@ return {
   init_options = { documentFormatting = false }, -- done by stylua
   settings = {
     Lua = {
+      telemetry = { enable = false },
       runtime = {
         version = "LuaJIT",
       },
       diagnostics = {
         globals = { "vim" },
+        disable = {
+          -- Need check nil
+          "need-check-nil",
+          -- This function requires 2 argument(s) but instead it is receiving 1
+          "missing-parameter",
+          -- Cannot assign `unknown` to `string`.
+          "assign-type-mismatch",
+          -- Cannot assign `unknown` to parameter `string`.
+          "param-type-mismatch",
+          -- This variable is defined as type `string`. Cannot convert its type to `unknown`.
+          "cast-local-type",
+        },
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
         library = vim.api.nvim_get_runtime_file("", true),
+        -- library = {
+        --   [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+        --   [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+        -- },
         maxPreload = 3000,
       },
-      telemetry = { enable = false },
+      -- disable certain warnings that don't concern us
+      -- https://github.com/sumneko/lua-language-server/blob/master/doc/en-us/config.md
+      type = {
+        -- Cannot assign `string|nil` to parameter `string`.
+        weakNilCheck = true,
+        weakUnionCheck = true,
+        -- Cannot assign `number` to parameter `integer`.
+        castNumberToInteger = true,
+      },
     },
   },
 }
