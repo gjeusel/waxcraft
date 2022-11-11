98eb6ab15aca22007c386ef7fe564eeb45d3008f
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2022-11-11 22:54:28 +0100

    add(nvim): common typos correct

--- dotfiles/.config/nvim/lua/wax/keymaps.lua
+++ dotfiles/.config/nvim/lua/wax/keymaps.lua
@@ -1,5 +1,29 @@
 local kmap = vim.keymap.set
 
+-- Fix common typos
+vim.cmd([[
+    cnoreabbrev W! w!
+    cnoreabbrev W1 w!
+    cnoreabbrev w1 w!
+    cnoreabbrev Q! q!
+    cnoreabbrev Q1 q!
+    cnoreabbrev q1 q!
+    cnoreabbrev Qa! qa!
+    cnoreabbrev Qall! qall!
+    cnoreabbrev Wa wa
+    cnoreabbrev Wq wq
+    cnoreabbrev wQ wq
+    cnoreabbrev WQ wq
+    cnoreabbrev wq1 wq!
+    cnoreabbrev Wq1 wq!
+    cnoreabbrev wQ1 wq!
+    cnoreabbrev WQ1 wq!
+    cnoreabbrev W w
+    cnoreabbrev Q q
+    cnoreabbrev Qa qa
+    cnoreabbrev Qall qall
+]])
+
 --------- Behaviour fixes ---------
 kmap("i", "<C-e>", "<End>")
 kmap("i", "<C-a>", "<Home>")
