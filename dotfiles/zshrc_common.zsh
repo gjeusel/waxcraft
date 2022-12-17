f2f08a2829acbf77aac6340bfc38758363f456c7
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2022-12-17 11:46:31 +0100

    imp(zsh)

--- dotfiles/zshrc_common.zsh
+++ dotfiles/zshrc_common.zsh
@@ -17,9 +17,14 @@ source "$waxCraft_PATH/dotfiles/fzf-extras.zsh"
 # History
 setopt appendhistory
 setopt inc_append_history
-setopt share_history
+setopt share_history           # Share history accross shells
 
-#setopt hist_ignore_all_dups hist_ignore_dups hist_expire_dups_first
+setopt hist_expire_dups_first  # Expire a duplicate event first when trimming history.
+setopt hist_ignore_dups        # Do not record an event that was just recorded again.
+setopt hist_ignore_all_dups    # Delete an old recorded event if a new event is a duplicate.
+setopt hist_find_no_dups       # Do not display a previously found event.
+setopt hist_save_no_dups       # Do not write a duplicate event to the history file.
+
 #setopt hist_reduce_blanks hist_ignore_space
 #setopt hist_reduce_blanks hist_ignore_space hist_verify
 
@@ -31,28 +36,40 @@ setopt share_history
 
 # _______ ZSH Modules config _______
 
+# ctrl-x to edit current command in $EDITOR:
 autoload -U edit-command-line
 zle -N edit-command-line
 
 autoload -U select-word-style
 select-word-style bash
 
+# Add Vi text-objects for brackets and quotes
+autoload -Uz select-bracketed select-quoted
+zle -N select-quoted
+zle -N select-bracketed
+for km in viopp visual; do
+  bindkey -M $km -- '-' vi-up-line-or-history
+  for c in {a,i}${(s..)^:-\'\"\`\|,./:;=+@}; do
+    bindkey -M $km $c select-quoted
+  done
+  for c in {a,i}${(s..)^:-'()[]{}<>bB'}; do
+    bindkey -M $km $c select-bracketed
+  done
+done
+
 # --- Autocompletion ---
 
-# versioned completions (docker + docker-compose):
+# our own completions for docker + docker-compose:
 fpath+="$waxCraft_PATH/dotfiles/completions"
 
 # shell user completion:
-
-# create ~/.zfunc if missing
-if [[ ! -d $HOME/.zfunc ]]; then
-  mkdir ~/.zfunc
+if [[ td ${ZDOTDIR:-~}/.zfunc ]]; then
+  fpath+="${ZDOTDIR:-~}/.zfunc"
 fi
-fpath+="$HOME/.zfunc"
 
 # auto generate kubectl completion if needed:
-if [[ (! -f $HOME/.zfunc/_kubectl) && (( $+commands[kubectl] ))]]; then
-  kubectl completion zsh &> $HOME/.zfunc/_kubectl
+if [[ (! -f ${ZDOTDIR:-~}/.zfunc/_kubectl) && (( $+commands[kubectl] ))]]; then
+  kubectl completion zsh &> ${ZDOTDIR:-~}/.zfunc/_kubectl
 fi
 
 # brew and installed with brew completions:
@@ -82,15 +99,24 @@ compinit -i -C
 
 # --- Fine tuning ---
 # https://thevaluable.dev/zsh-completion-guide-examples/
+#
+# `zstyle <pattern> <style> <values>`
+#
+# pattern:
+# `:completion:<function>:<completer>:<command>:<argument>:<tag>`
 
 # enable cache:
 zstyle ':completion:*' use-cache on
-zstyle ':completion:*' cache-path "$HOME/.zcompcache"
+zstyle ':completion:*' cache-path "${ZDOTDIR:-~}/.zcompcache"
 
-# Ignore these everywhere except for rm
-zstyle ':completion:*:*:*' ignored-patterns '(|*/)__pycache__' \
-    '(|*/)*.egg-info' '(*/)#lost+found'
-zstyle ':completion:*:rm:*' ignored-patterns '(|*/)*.egg-info'
+local _ignored_patterns=(
+  '(|*/)__pycache__'
+  '(|*/)*.egg-info'
+  '(*/)#lost+found'
+)
+
+# Ignore these everywhere
+zstyle ':completion:*:*:*' ignored-patterns $_ignored_patterns
 
 # Set accept-exact-dirs for cases of mounted drives (Google Drive or S3-bucket)
 # to avoid slow down searching on parent directory
@@ -99,6 +125,17 @@ zstyle ':completion:*' accept-exact-dirs true
 
 # Ask for a menu instead of blindly cycling:
 zstyle ':completion:*' menu select
+
+# Add a title to the completion when specific commands
+zstyle ':completion:*:*:(ls|cd|cp):*:descriptions' format '%F{green}-- %d --%f'
+
+# Add info on number of errors corrected on completion
+zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
+
+# Add messages:
+zstyle ':completion:*:messages' format ' %F{magenta} -- %d --%f'
+zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
+
 
 # _______ Setups before plugin sourcing _______
 
@@ -140,6 +177,7 @@ source "$waxCraft_PATH/dotfiles/bindings.zsh"
 
 # _______ Fixes / Optims _______
 
+# Avoid issues trying to list all directories in a mounted S3
 # https://github.com/b4b4r07/enhancd/issues/85
 __enhancd::filter::exists()
 {
