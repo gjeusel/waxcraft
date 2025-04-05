44a6a0bb60a1405c12c6ad1b6de3a8274a2b4550
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2025-04-05 17:22:11 +0200

    upt(nix): activationScripts that works with default app for file ext

--- nix/flake.nix
+++ nix/flake.nix
@@ -42,7 +42,7 @@
         jq
         htop
         rclone
-        tldr
+        tealdeer
         wget
         bat
         dust
@@ -54,6 +54,8 @@
         # raycast
         aerospace
         # karabiner-elements
+
+        duti # help to switch default app handler for macos
 
         mountain-duck
 
@@ -67,6 +69,9 @@
         # ----- infra -----
         sops
         kubectl
+        (wrapHelm kubernetes-helm {
+          plugins = [kubernetes-helmPlugins.helm-secrets];
+        })
         terraform
         scaleway-cli
         python312Packages.pgcli
@@ -112,12 +117,17 @@
         yaml-language-server
 
         # ----- daily life -----
-        # calibre # unavailable for aarch64-darwin
         spotify
+        zoom-us # video conferencing application
 
         # ----- because I'm altruist -----
         vscode
         zed-editor
+
+        # ----- not available on aarch64-darwin (yet) -----
+        # calibre
+        # obs-studio
+        # vlc
       ];
 
       homebrew = {
@@ -132,7 +142,7 @@
 
           "brave-browser"
           "firefox"
-          "vlc" # unavailable in nixpkgs for aarch64-darwin
+          "vlc"
 
           "notion" # does not exists in nixpkgs
           "notion-calendar"
@@ -141,6 +151,10 @@
           "raycast"
           "karabiner-elements"
           # "nikitabobko/tap/aerospace" # failing, so for now manual...
+
+          "obs" # screen record
+
+          "cyberghost-vpn" # VPN
 
           "calibre" # e-book
         ];
@@ -162,34 +176,45 @@
         pkgs.nerd-fonts.hack
       ];
 
-      system.activationScripts = {
-        # Following should allow us to disable conflicting shortcuts.
-        postActivation.text = ''
-          # Disable Minimize:
-          defaults write -g NSUserKeyEquivalents -dict-add 'Minimize' '\0'
-        '';
+      # NOTE: - nix-darwin is only letting a limited activationScripts namespaces
+      #         see: https://github.com/nix-darwin/nix-darwin/blob/73d59580d01e9b9f957ba749f336a272869c42dd/modules/system/activation-scripts.nix#L149
+      #       - we use "postUserActivation" to not launch the commands as root, but rather as the user
+      system.activationScripts.postUserActivation.text = ''
+        # Disable Cmd+M minimize window shortcut
+        /usr/bin/defaults write -g NSUserKeyEquivalents -dict-add "Minimize" "\\0"
 
-        # Allow Raycast/Spotlight to find apps installed via nixpkgs brew:
-        applications.text = let
-          env = pkgs.buildEnv {
-            name = "system-applications";
-            paths = config.environment.systemPackages;
-            pathsToLink = "/Applications";
-          };
-        in
-          pkgs.lib.mkForce ''
-            # Set up applications.
-            echo "setting up /Applications..." >&2
-            rm -rf /Applications/Nix\ Apps
-            mkdir -p /Applications/Nix\ Apps
-            find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-            while read -r src; do
-              app_name=$(basename "$src")
-              echo "copying $src" >&2
-              ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-            done
-          '';
-      };
+        # --- Setup Default App for file extensions ---
+        # note: get the id with `$(osascript -e 'id of app "VLC"')`
+
+        # Set VLC as default for all common video formats
+        for ext in .3gp .3g2 .asf .avi .divx .dv .flv .m2t .m2ts .m4v .mkv \
+                   .mov .mp4 .mpeg .mpg .ogm .ogv .qt .rm .rmvb .ts .vob \
+                   .webm .wmv .xvid .amv .dav .f4v .hevc .m1v .m2v .m4b \
+                   .mxf .nsv .rec .swf .tod; do
+          ${pkgs.duti}/bin/duti -s org.videolan.vlc "$ext" all
+        done
+
+        # Set VLC as default for all common audio formats
+        for ext in .mp3 .flac .wav .aiff .aif .ogg .m4a .wma .opus .alac .aac \
+                   .ac3 .amr .ape .au .cda .dts .mka .mid .midi .mp2 .mpa .mpc \
+                   .ra .rmi .spx .tta .wv .weba .pcm .dsf .m4b .m4r .webm; do
+          ${pkgs.duti}/bin/duti -s org.videolan.vlc "$ext" all
+        done
+
+        # Set Zed as default for all common text file formats
+        for ext in .txt .md .markdown .rst .org .tex .bib .log .csv .tsv \
+                   .json .yaml .yml .xml .html .htm .css .scss .sass .less \
+                   .js .jsx .ts .tsx .mjs .cjs .php .py .rb .go .java .kt \
+                   .scala .swift .m .h .c .cpp .cc .hpp .cs .fs .fsx .rs \
+                   .lua .pl .pm .sh .bash .zsh .fish .ps1 .bat .cmd .vbs \
+                   .ini .cfg .conf .config .toml .env .gitignore .dockerignore \
+                   .editorconfig .eslintrc .prettierrc .babelrc .npmrc \
+                   .lock .gitattributes .gitmodules .gradle .properties \
+                   .pug .jade .ejs .erb .haml .slim .mustache .hbs .handlebars \
+                   .vue .svelte .astro .graphql .gql .sql .prisma; do
+          ${pkgs.duti}/bin/duti -s dev.zed.Zed "$ext" all
+        done
+      '';
 
       system.defaults = {
         menuExtraClock.Show24Hour = true; # show 24 hour clock
