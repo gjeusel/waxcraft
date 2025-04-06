2e214c60e5c1d9e472dfc04d7030b7997d165415
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2025-04-06 11:32:39 +0200

    fix(nix): add back nixpkgs to spotlight alias

--- nix/system-scripts.nix
+++ nix/system-scripts.nix
@@ -42,4 +42,25 @@
     #   ${pkgs.duti}/bin/duti -s dev.zed.Zed "$ext" all
     # done
   '';
+
+  # Make Apps installed with nixpkgs appear in spotlight search (by making alias in /Applications/Nix Apps/)
+  system.activationScripts.applications.text = let
+    env = pkgs.buildEnv {
+      name = "system-applications";
+      paths = config.environment.systemPackages;
+      pathsToLink = "/Applications";
+    };
+  in
+    pkgs.lib.mkForce ''
+      # Set up applications.
+      echo "setting up /Applications..." >&2
+      rm -rf /Applications/Nix\ Apps
+      mkdir -p /Applications/Nix\ Apps
+      find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+      while read -r src; do
+        app_name=$(basename "$src")
+        echo "copying $src" >&2
+        ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+      done
+    '';
 }
