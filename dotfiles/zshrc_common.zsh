e88335dba4f1223bfa196e044b31d47a65761b5c
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2025-04-07 14:45:26 +0200

    upt(zsh): remove sudo snippet

--- dotfiles/zshrc_common.zsh
+++ dotfiles/zshrc_common.zsh
@@ -172,7 +172,7 @@ zinit light Aloxaf/fzf-tab                          # fzf for tab completion
 zstyle ':fzf-tab:*' fzf-flags --color fg:#e5e9f0,bg+:#3b4252,hl:#81a1c1,fg+:#e5e9f0,bg+:#3b4252,hl+:#81a1c1,info:#eacb8a,prompt:#bf6069,pointer:#b48dac,marker:#a3be8b,spinner:#b48dac,header:#a3be8b,gutter:-1 --pointer '❯'
 
 # snippets
-for snip in git extract common-aliases tmux sudo command-not-found gcloud aws kubectl kubectx; do
+for snip in git extract common-aliases tmux command-not-found gcloud aws kubectl kubectx; do
     zinit ice wait lucid
     zinit snippet OMZP::$snip
 done
