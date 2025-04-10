e7b287a3ad32fd2a1f8d90fb269e05317453edc8
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2025-04-10 11:00:42 +0200

    add(nvim): codeblock snippet for all

--- dotfiles/nvim/.config/nvim/lua/wax/snippets/all.lua
+++ dotfiles/nvim/.config/nvim/lua/wax/snippets/all.lua
@@ -1,5 +1,10 @@
 local ls = require("luasnip")
 
+local fmt = require("luasnip.extras.fmt").fmt
+
+local s = ls.snippet
+local i = ls.insert_node
+
 return {
   ls.parser.parse_snippet("$file$", "$TM_FILENAME"),
   ls.parser.parse_snippet("$uuid$", '"$UUID"'),
@@ -7,4 +12,17 @@ return {
   ls.parser.parse_snippet("$date$", os.date("%Y-%m-%d")),
   ls.parser.parse_snippet("$time$", os.date("%H:%M")),
   ls.parser.parse_snippet("$datetime$", os.date("%Y-%m-%dT%H:%M:%S")),
+
+  -- codeblocks
+  s(
+    "cbash",
+    fmt(
+      [[
+        ```bash
+        {code}
+        ```
+      ]],
+      { code = i(0, "") }
+    )
+  ),
 }
