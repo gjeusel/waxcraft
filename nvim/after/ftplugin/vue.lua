ad5ee211a6bcaeee6f540212d1994e3b8d458ebf
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2025-06-06 17:29:48 +0200

    fix(nvim): flickering of treesitter for vue

--- nvim/after/ftplugin/vue.lua
+++ nvim/after/ftplugin/vue.lua
@@ -3,3 +3,7 @@ vim.opt_local.colorcolumn = "120" -- Show vertical bar at column 120
 
 -- remove <:> from matchpairs as we use html tags
 vim.o.matchpairs = "(:),{:},[:],':',\":\""
+
+
+-- https://github.com/neovim/neovim/issues/32660
+vim.g._ts_force_sync_parsing = true
