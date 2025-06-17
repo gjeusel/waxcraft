8b3e02ad34e80a685b33857571c37fb50e0c2472
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2025-06-17 21:48:42 +0200

    add(nvim): jinja ft

---
+++ nvim/after/ftplugin/jinja.lua
@@ -0,0 +1,11 @@
+vim.opt_local.foldmethod = "indent"
+vim.opt_local.wrap = false
+
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
+
+vim.opt_local.wrap = true
+
+vim.opt_local.commentstring = "{# %s #}"
+-- vim.opt_local.comments = "{#"
