eadfa0756ddb1b6787337be04c7e5ea0b6a50865
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2025-09-20 11:40:43 +0200

    upt(nvim): cleanup autocmds

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -1,3 +1,23 @@
 vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 vim.opt_local.softtabstop = 4
+
+vim.g.python3_host_prog = require("wax.lsp.python-utils").get_python_path()
+
+local utils = require("wax.utils")
+
+vim.keymap.set("n", "<leader>o", function()
+  utils.insert_new_line_in_current_buffer('__import__("pdb").set_trace()  # BREAKPOINT')
+end, {
+  buffer = 0,
+  desc = "Insert pdb breakpoint below.",
+})
+vim.keymap.set("n", "<leader>O", function()
+  utils.insert_new_line_in_current_buffer(
+    '__import__("pdb").set_trace()  # BREAKPOINT',
+    { delta = 0 }
+  )
+end, {
+  buffer = 0,
+  desc = "Insert pdb breakpoint above.",
+})
--- nvim/lua/wax/autocmds.lua
+++ nvim/lua/wax/autocmds.lua
@@ -3,16 +3,20 @@ local utils = require("wax.utils")
 -- Handle Views
 local group_view = "Views"
 vim.api.nvim_create_augroup(group_view, { clear = true })
-vim.api.nvim_create_autocmd({ "BufWinEnter" }, { pattern = "*", command = "silent! loadview" })
+vim.api.nvim_create_autocmd(
+  { "BufWinEnter" },
+  { desc = "Load view", pattern = "*", command = "silent! loadview" }
+)
 vim.api.nvim_create_autocmd(
   { "BufWrite", "BufLeave" },
-  { pattern = "*", command = "silent! mkview" }
+  { desc = "Save view", pattern = "*", command = "silent! mkview" }
 )
 
 -- Diff
 local previous_foldmethod = nil
 
 vim.api.nvim_create_autocmd("OptionSet", {
+  desc = "Disable folds & views in diff buffers",
   pattern = "diff",
   callback = function()
     if vim.opt.diff then
@@ -29,38 +33,9 @@ vim.api.nvim_create_autocmd("OptionSet", {
   end,
 })
 
--- Python
+-- Frontend keymaps
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "python",
-  callback = function()
-    vim.keymap.set("n", "<leader>o", function()
-      utils.insert_new_line_in_current_buffer('__import__("pdb").set_trace()  # BREAKPOINT')
-    end, {
-      buffer = 0,
-      desc = "Insert pdb breakpoint below.",
-    })
-    vim.keymap.set("n", "<leader>O", function()
-      utils.insert_new_line_in_current_buffer(
-        '__import__("pdb").set_trace()  # BREAKPOINT',
-        { delta = 0 }
-      )
-    end, {
-      buffer = 0,
-      desc = "Insert pdb breakpoint above.",
-    })
-  end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "python",
-  callback = function()
-    local python = require("wax.lsp.python-utils").get_python_path()
-    vim.g.python3_host_prog = python
-  end,
-})
-
--- Frontend
-vim.api.nvim_create_autocmd("FileType", {
+  desc = "Add keymaps for debugger breakpoints for frontend files",
   pattern = {
     "vue",
     "svelte",
@@ -86,36 +61,11 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
--- -- Fix Html like mini.ai
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = { "jinja.html", "html", "vue" },
---   callback = function()
---     -- Solve issue on mini ai with nested html tags
---     -- https://github.com/echasnovski/mini.nvim/issues/110
---     if is_module_available("mini.ai") then
---       local spec_treesitter = require("mini.ai").gen_spec.treesitter
---       vim.b.miniai_config = {
---         custom_textobjects = {
---           t = spec_treesitter({ a = "@function.outer", i = "@function.inner" }),
---         },
---       }
---     end
---   end,
--- })
-
--- -- Can't use after file for jinja.html filetype
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = { "jinja.html" },
---   callback = function()
---     -- vim.opt_local.commentstring = "{# %s #}"
---     -- vim.opt_local.comments = "{#"
---   end,
--- })
-
 -- Performances
 --
 -- https://www.reddit.com/r/neovim/comments/pz3wyc/comment/heyy4qf/?utm_source=share&utm_medium=web2x&context=3
 vim.api.nvim_create_autocmd({ "BufReadPre", "FileReadPre" }, {
+  desc = "Optimize performances in big files",
   pattern = "*",
   callback = function(opts)
     local fpath = opts.match
@@ -123,7 +73,6 @@ vim.api.nvim_create_autocmd({ "BufReadPre", "FileReadPre" }, {
       return
     end
 
-    local fname = vim.fn.expand("%:t")
     vim.schedule(function()
       print("big file detected -> minimalist mode")
     end)
