631dc08132b376d788fb622c5d24d53b88aacba5
Jeusel Guillaume <jeusel.guillaume@gmail.com>
2026-07-25 08:53:10 +0200

    Revert "upt(nix): system scripts for exts"
    This reverts commit 36b520e4d3066be422fbd32d52fc1ce33a0f6ed1.

--- nix/system-scripts.nix
+++ nix/system-scripts.nix
@@ -15,55 +15,40 @@
 
     # --- Setup Default App for file extensions ---
     # note: get the id with `osascript -e 'id of app "VLC"'
-    #
-    # macOS 26.4+ shows a confirmation dialog for every programmatic
-    # default-app *change*, and refuses extensions that only resolve to a
-    # dynamic UTI (error -50). Only call duti when the current handler
-    # differs, so rebuilds are silent no-ops once associations are in place.
-    skipped=""
-    set_handler() {
-      bundle="$1"
-      shift
-      for ext in "$@"; do
-        current="$(${pkgs.duti}/bin/duti -x "''${ext#.}" 2>/dev/null | /usr/bin/sed -n 3p)"
-        if [ "$current" != "$bundle" ]; then
-          ${pkgs.duti}/bin/duti -s "$bundle" "$ext" all 2>/dev/null || skipped="$skipped $ext"
-        fi
-      done
-    }
 
-    # VLC: common video formats (.ts belongs to Zed, TypeScript)
-    set_handler org.videolan.vlc \
-      .3gp .3g2 .asf .avi .divx .dv .flv .m2t .m2ts .m4v .mkv \
-      .mov .mp4 .mpeg .mpg .ogm .ogv .qt .rm .rmvb .vob \
-      .webm .wmv .xvid .amv .dav .f4v .hevc .m1v .m2v .m4b \
-      .mxf .nsv .rec .swf .tod
+    # Set VLC as default for all common video formats
+    for ext in .3gp .3g2 .asf .avi .divx .dv .flv .m2t .m2ts .m4v .mkv \
+               .mov .mp4 .mpeg .mpg .ogm .ogv .qt .rm .rmvb .ts .vob \
+               .webm .wmv .xvid .amv .dav .f4v .hevc .m1v .m2v .m4b \
+               .mxf .nsv .rec .swf .tod; do
+      ${pkgs.duti}/bin/duti -s org.videolan.vlc "$ext" all
+    done
 
-    # VLC: common audio formats
-    set_handler org.videolan.vlc \
-      .mp3 .flac .wav .aiff .aif .ogg .m4a .wma .opus .alac .aac \
-      .ac3 .amr .ape .au .cda .dts .mka .mid .midi .mp2 .mpa .mpc \
-      .ra .rmi .spx .tta .wv .weba .pcm .dsf .m4b .m4r .webm
+    # Set VLC as default for all common audio formats
+    for ext in .mp3 .flac .wav .aiff .aif .ogg .m4a .wma .opus .alac .aac \
+               .ac3 .amr .ape .au .cda .dts .mka .mid .midi .mp2 .mpa .mpc \
+               .ra .rmi .spx .tta .wv .weba .pcm .dsf .m4b .m4r .webm; do
+      ${pkgs.duti}/bin/duti -s org.videolan.vlc "$ext" all
+    done
 
-    # Zed: common text file formats
-    set_handler dev.zed.Zed \
-      .txt .md .markdown .rst .org .tex .bib .log \
-      .json .yaml .yml .xml .css .scss .sass .less \
-      .js .jsx .ts .tsx .mjs .cjs .php .py .rb .go .java .kt \
-      .scala .swift .m .h .c .cpp .cc .hpp .cs .fs .fsx .rs \
-      .lua .pl .pm .sh .bash .zsh .fish .ps1 .bat .cmd .vbs \
-      .ini .cfg .conf .config .toml .env .gitignore .dockerignore \
-      .editorconfig .eslintrc .prettierrc .babelrc .npmrc \
-      .lock .gitattributes .gitmodules .gradle .properties \
-      .pug .jade .ejs .erb .haml .slim .mustache .hbs .handlebars \
-      .vue .svelte .astro .graphql .gql .sql .prisma
+    # Set Zed as default for all common text file formats
+    for ext in .txt .md .markdown .rst .org .tex .bib .log \
+               .json .yaml .yml .xml .css .scss .sass .less \
+               .js .jsx .ts .tsx .mjs .cjs .php .py .rb .go .java .kt \
+               .scala .swift .m .h .c .cpp .cc .hpp .cs .fs .fsx .rs \
+               .lua .pl .pm .sh .bash .zsh .fish .ps1 .bat .cmd .vbs \
+               .ini .cfg .conf .config .toml .env .gitignore .dockerignore \
+               .editorconfig .eslintrc .prettierrc .babelrc .npmrc \
+               .lock .gitattributes .gitmodules .gradle .properties \
+               .pug .jade .ejs .erb .haml .slim .mustache .hbs .handlebars \
+               .vue .svelte .astro .graphql .gql .sql .prisma; do
+      ${pkgs.duti}/bin/duti -s dev.zed.Zed "$ext" all
+    done
 
-    # Excel: common table formats
-    set_handler com.microsoft.Excel .csv .tsv .xlsx
-
-    if [ -n "$skipped" ]; then
-      echo "duti: could not set:$skipped (dynamic UTI, or confirmation dialog declined)"
-    fi
+    # Set Excel as default for common table formats
+    for ext in .csv .tsv .xlsx; do
+      ${pkgs.duti}/bin/duti -s com.microsoft.Excel "$ext" all
+    done
   '';
 in {
   system.activationScripts.postActivation.text = ''
